116cdcf37ffb243c681ed783fe9cca438cf2802d
srikargudimella <111258947+srikargudimella@users.noreply.github.com>
2024-11-11 15:48:43 -0500

    Add React routing

--- jamcloud/src/App.js
+++ jamcloud/src/App.js
@@ -1,17 +1,18 @@
-// import logo from './logo.svg';
 import './App.css';
-
 import React from 'react';
-import  CommentsPage from './components/CommentSection/CommentsPage.tsx'; 
-// import TrackView from './components/TracksPage/TrackView.tsx';
-// import "./components/CommentSection/MyComponent.tsx";
-
-// import { MyComponent } from '.components/Comments/MyComponent/index';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CommentsPage from './components/CommentSection/CommentsPage.tsx';
+import TracksPage from './components/TracksPage/TracksPage.tsx';
 
 function App() {
   return (
     <div className="App">
-      <CommentsPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<TracksPage />} />
+          <Route path="/comments" element={<CommentsPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
